b761d20e3f3aca1d75fb1b0412c5ce288a418a86
JoeFikis <aidanmg608@gmail.com>
2023-01-11 18:28:00 -0600

    work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
 import Navbar from './Components/Navbar/Navbar'
 import { BrowserRouter as Router , Route, Routes } from "react-router-dom";
+
+import Home from './Components/Pages/Home/Home';
+import About from './Components/Pages/About/About';
 
 function App() {
   return (
     <div>
       <Navbar />
-
       <Router>
         <Routes>
-          <Route path='/'></Route>
-          <Route path='/about'></Route>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About/>}/>
         </Routes>
       </Router>
     </div>
